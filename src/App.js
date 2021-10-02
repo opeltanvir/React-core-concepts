48d79caaf89c661e2ded72522463b9053ef943a9
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-10-02 22:16:58 +0600

    core

--- src/App.js
+++ src/App.js
@@ -2,24 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks =['Anower','jafor','alomgir','salman']
+  const products =[
+    {name:'Photoshop',price:'$90.99'},
+    {name:'Illustrator', price:'60.99'},
+    {name:'PDF Reader',price:'$6.99'}
+]
+const productNames = products.map(product => product.name)
+console.log(productNames);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I am a React Person</p>
+        <Product product={products[0]}></Product>
+        <Person name="Munna" job="football"></Person>
+        <Person name="Masum" job="cricket"></Person>
       </header>
     </div>
   );
+}
+function Product(props){
+  const productStyle={
+    border: '1px solid gray',
+    borderRadius:'5px',
+    backgroundColor:'lightgray',
+    height:'200px',
+    width:'200px',
+    float:'left'
+  }
+ 
+  return(
+    <div style={productStyle}>
+      <h3>{props.product.name}</h3>
+      <h4>{props.product.price}</h4>
+      <button>Buy now</button>
+    </div>
+  )
+}
+function Person(props){
+  return (
+    <div style= {{border:'2px solid gold',width:'400px'}}>
+      <h3>My Name:{props.name}</h3>
+      <p>My Profession:{props.job}</p>
+    </div>
+  )
 }
 
 export default App;
